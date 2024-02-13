c1a65473c8f36bf3e895115fe20d51c70070305c
june-seok-kim <purifier772@gmail.com>
2024-02-13 22:56:16 +0900

    Changed title for faulty emoji

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "? Sorry, Codes",
+    pageTitle: "Sorry, Codes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
